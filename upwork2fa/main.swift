4eb7d50aa6a49d1e086a6763e0fe21a88e7cc80f
Max Desiatov <max@desiatov.com>
2022-05-29 11:42:50 +0100

    Refactor CSV conversion code

--- upwork2fa/main.swift
+++ upwork2fa/main.swift
@@ -37,52 +37,53 @@ enum TransactionType: String {
   case serviceFee = "Service Fee"
   case hourly = "Hourly"
   case withdrawal = "Withdrawal"
+  case bonus = "Bonus"
 }
 
-guard
-  let first = CommandLine.arguments.first,
-  let last = CommandLine.arguments.last,
-  first != last
-else {
-  fatalError("file argument not passed")
-}
+func convert(at filePath: String) throws {
+  let inputStream = InputStream(fileAtPath: filePath)!
+  let csv = try! CSVReader(stream: inputStream)
 
-let inputStream = InputStream(fileAtPath: last)!
-let csv = try! CSVReader(stream: inputStream)
+  var lastServiceFee: Decimal?
+  var lastServiceFeeDesc: String?
+  var results = [(Date, Decimal, String)]()
 
-var lastServiceFee: Decimal?
-var lastServiceFeeDesc: String?
-var results = [(Date, Decimal, String)]()
+  for row in csv.dropFirst().reversed() {
+    let date = inputFormatter.date(from: row[0])!
+    let type = TransactionType(rawValue: row[2])!
+    var amount = Decimal(string: row[9])!
+    let description: String
 
-for row in csv.dropFirst().reversed() {
-  let date = inputFormatter.date(from: row[0])!
-  let type = TransactionType(rawValue: row[2])!
-  var amount = Decimal(string: row[9])!
-  let description: String
+    switch type {
+    case .vat:
+      description = lastServiceFeeDesc!
+      amount += lastServiceFee!
+    case .serviceFee:
+      lastServiceFee = amount
+      lastServiceFeeDesc = row[3]
+      continue
+    case .withdrawal:
+      description = type.rawValue + " " + row[3]
+    case .hourly, .bonus:
+      description = row[3]
+      lastServiceFee = nil
+      lastServiceFeeDesc = nil
+    }
 
-  switch type {
-  case .vat:
-    description = lastServiceFeeDesc!
-    amount += lastServiceFee!
-  case .serviceFee:
-    lastServiceFee = amount
-    lastServiceFeeDesc = row[3]
-    continue
-  case .withdrawal:
-    description = type.rawValue + " " + row[3]
-  case .hourly:
-    description = row[3]
-    lastServiceFee = nil
-    lastServiceFeeDesc = nil
+    let result = (date, amount, description)
+    results.append(result)
   }
 
-  let result = (date, amount, description)
-  results.append(result)
+  let fileURL = URL(fileURLWithPath: filePath)
+  let fileName = String(fileURL.lastPathComponent.dropLast(4))
+  let outputStream = OutputStream(toFileAtPath: fileURL.deletingLastPathComponent().appendingPathComponent("\(fileName)-freeagent.csv").path, append: false)!
+  let writer = try CSVWriter(stream: outputStream)
+
+  for (date, amount, description) in results {
+    try writer.write(row: [outputFormatter.string(from: date), decimalFormatter.string(from: amount as NSNumber)!, description])
+  }
 }
 
-let outputStream = OutputStream(toFileAtPath: URL(fileURLWithPath: last).deletingLastPathComponent().appendingPathComponent("freeagent.csv").path, append: false)!
-let writer = try CSVWriter(stream: outputStream)
-
-for (date, amount, description) in results {
-  try writer.write(row: [outputFormatter.string(from: date), decimalFormatter.string(from: amount as NSNumber)!, description])
+for f in [] {
+  try convert(at: "/\(f)")
 }
